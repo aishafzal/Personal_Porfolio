285a5b002ace0be8bcfa68d5e18a58c89cfc980a
AISHA <aishaafzal157@gmail.com>
2023-08-31 04:26:34 +0500

    completed responsive

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './portfolio.css'
+import minifoodpanda from '../../img/minifoodpanda.JPG'
+import chatapp from '../../img/chatapp.JPG'
+import digihealth from '../../img/digihealth.JPG'
+import digihealth1 from '../../img/digihealth1.JPG'
+import product from '../../img//product.JPG'
+import product1 from '../../img/productdev2.JPG'
+import digi from '../../img/digih.JPG'
+import e1 from '../../img/ecommerce1.JPG';
+import e2 from '../../img/ecommerce2.JPG';
+import e3 from '../../img/ecoomerce3.JPG';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import { themeContext } from '../../Context';
+import { useContext } from 'react';
+
+const Portfolio = () => {
+    const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className='portfolio' id="Portfolio">
+      <span style={{color:darkMode? 'white': ''}}>Recent Projects</span>
+      <span>Portfolio</span>
+
+    <Swiper
+    spaceBetween={30}
+    slidesPerView={3}
+    grabCursor={true}
+    className='portfolio-slider'
+    >
+<SwiperSlide>
+    <img src={digi} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={digihealth} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={digihealth1} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={e1} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={e2} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={e3} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={chatapp} alt=""></img>
+</SwiperSlide>
+
+<SwiperSlide>
+    <img src={minifoodpanda} alt=""></img>
+</SwiperSlide>
+<SwiperSlide>
+    <img src={product1} alt=""></img>
+</SwiperSlide>
+
+<SwiperSlide>
+    <img src={product} alt=""></img>
+</SwiperSlide>
+
+</Swiper>
+    </div>
+  )
+}
+
+export default Portfolio
